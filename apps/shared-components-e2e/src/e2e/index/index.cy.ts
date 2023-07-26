0297a2baabe94e13b73615620a49b00c4eb8d550
o.matyka@andersenlab.com <o.matyka@andersenlab.com>
2023-07-26 20:28:41 +0300

    2

--- apps/shared-components-e2e/src/e2e/index/index.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('shared-components: SharedComponents component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=sharedcomponents--primary'));
-
-  it('should render the component', () => {
-    cy.get('h1').should('contain', 'Welcome to shared-components!');
-  });
-});
